769550a9e70249c5b19cb836f783a873af303300
Ahmad Nasser <ahmad_nasser04@outlook.com>
2023-03-20 12:05:08 +0200

    tsx back

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Layout } from "../components";
 import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
