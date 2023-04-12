d2a7bc5254d6a4bdb93dcd231a6908a2cabf0c6b
Ahmad Nasser <ahmad_nasser04@outlook.com>
2023-04-12 06:00:48 +0300

    analyrics og

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Layout } from "../components";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
